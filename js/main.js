5387121ab2e08eaffe35579cd2474359a190373e
Manuel Garcia <manuelgarcia@Manuels-MacBook-Pro.local>
2017-07-19 13:02:04 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "King";
+
+console.log("user flipped" + " " + cardOne);
+console.log("user flipped" + " " + cardThree);
