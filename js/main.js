db6be6cffc38448d2a798809b0ad7b0167c7f325
Manuel Garcia <manueldjg@ymail.com>
2017-07-23 13:29:50 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 console.log("Up and Running!");
 
 
+
+
+
+
 var cards = ['queen','queen','king','king'];
 // alert('Game is up and running!');
 
+var cardsInPlay = [];
 var cardOne = cards[0];
 var cardTwo = cards[2];
 
-var cardsInPlay = [];
-cardsInPlay.push(cardOne);
-// console.log(cardsInPlay);
-cardsInPlay.push(cardTwo);
-// console.log(cardsInPlay);
+var checkForMatch = function() {
+	if (cardsInPlay.length === 2) {
 
-if (cardsInPlay.length === 2) {
-
-	if (cardsInPlay[0] === cardsInPlay[2]) {
-		alert('You found a match!');
-	} else if (cardsInPlay[0] !== cardsInPlay[2]) {
-		alert("sorry try again!");
+		if (cardsInPlay[0] === cardsInPlay[2]) {
+			alert('You found a match!');
+		} else if (cardsInPlay[0] !== cardsInPlay[2]) {
+			alert("sorry try again!");
+		}
 	}
+
 }
+
+var flipCard = function(cardId) {
+	console.log("user flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
+	checkForMatch()
+} 
+flipCard(0)
+flipCard(2)
+
+
+
 
 
 
@@ -29,6 +42,4 @@ if (cardsInPlay.length === 2) {
 
 
 
-console.log("User Flipped" + " " + "queen");
-console.log("User Flipped" + " " + "king");
 
