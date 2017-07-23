7a576772977f7bdde5ac6d1809383becedbbcc86
Manuel Garcia <manueldjg@ymail.com>
2017-07-23 15:45:02 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-console.log("Up and Running!");
+// console.log("Up and Running!");
 
 
 
 
 
 
-var cards = ['queen','queen','king','king'];
-// alert('Game is up and running!');
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}	
+];
+
 
 var cardsInPlay = [];
 var cardOne = cards[0];
@@ -25,8 +46,10 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-	console.log("user flipped " + cards[cardId])
-	cardsInPlay.push(cards[cardId])
+	console.log("user flipped " + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank)
+	console.log("cardImage " + cards[cardId].cardImage)
+	console.log("cardImage " + cards[cardId].suit)
 	checkForMatch()
 } 
 flipCard(0)
